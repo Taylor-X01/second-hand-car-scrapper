13df9b74e3b95c19694767d13a9e335dd3c28d0d
AnassGrini <53356479+Taylor-X01@users.noreply.github.com>
2021-05-15 10:25:15 +0000

    Changes models.py

--- pfa/models.py
+++ pfa/models.py
@@ -3,11 +3,9 @@ from django.db.models.fields import CharField
 
 class car_data(models.Model):
     # Object fields
-    brand   = models.CharField(max_length=50)
+    brand   = models.CharField(max_length=100)
     model   = models.CharField(max_length=50)
     year    = models.DecimalField()
     km_min  = models.DecimalField()
     km_max  = models.DecimalField()
     city    = models.CharField(max_length=50)
-    
-
