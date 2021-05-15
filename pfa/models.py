b0df52d74b6452074e55803b18dc85d2ce6172be
AnassGrini <53356479+Taylor-X01@users.noreply.github.com>
2021-05-15 10:11:21 +0000

    Add models.py

---
+++ pfa/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+class car_data(models.Model):
+    # Object fields
+    brand   = models.CharField(max_length=50)
+    model   = models.CharField(max_length=50)
+    year    = models.DecimalField()
+    km_min  = models.DecimalField()
+    km_max  = models.DecimalField()
+    city    = models.CharField(max_length=50)
+    
+
